3b55a850e519527b2499675e3c6b8e2e84ba7012
SzentgyorgyiBernad <szentgyorgyi.bernad@student.ms.sapientia.ro>
2022-03-02 22:32:38 +0200

    labor2

--- labor-02/src/oop/labor02/date/Main.java
+++ labor-02/src/oop/labor02/date/Main.java
@@ -4,40 +4,38 @@ import java.time.Year;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println(DateUtil.isValidDate(2000,2, 29) == true);
-        System.out.println(DateUtil.isValidDate(2000,2, 30) == false);
-        System.out.println(DateUtil.isValidDate(1900,2, 29) == false);
-        System.out.println(DateUtil.isValidDate(1900,2, 28) == true);
-        System.out.println(DateUtil.isValidDate(-1900,2, 28) == false);
-        System.out.println(DateUtil.isValidDate(0,2, 28) == false);
-        System.out.println(DateUtil.isValidDate(2021,2, 29) == false);
-        System.out.println(DateUtil.isValidDate(2020,2, 29) == true);
-        System.out.println(DateUtil.isValidDate(2020,1, 32) == false);
-        System.out.println(DateUtil.isValidDate(2020,1, 0) == false);
-        System.out.println(DateUtil.isValidDate(2020,0, 0) == false);
-        System.out.println(DateUtil.isValidDate(2020,4, 31) == false);
-        System.out.println(DateUtil.isValidDate(2020,1, 31) == true);
+    public static void main(String[] args) {
+        System.out.println(DateUtil.isValidDate(2000, 2, 29) == true);
+        System.out.println(DateUtil.isValidDate(2000, 2, 30) == false);
+        System.out.println(DateUtil.isValidDate(1900, 2, 29) == false);
+        System.out.println(DateUtil.isValidDate(1900, 2, 28) == true);
+        System.out.println(DateUtil.isValidDate(-1900, 2, 28) == false);
+        System.out.println(DateUtil.isValidDate(0, 2, 28) == false);
+        System.out.println(DateUtil.isValidDate(2021, 2, 29) == false);
+        System.out.println(DateUtil.isValidDate(2020, 2, 29) == true);
+        System.out.println(DateUtil.isValidDate(2020, 1, 32) == false);
+        System.out.println(DateUtil.isValidDate(2020, 1, 0) == false);
+        System.out.println(DateUtil.isValidDate(2020, 0, 0) == false);
+        System.out.println(DateUtil.isValidDate(2020, 4, 31) == false);
+        System.out.println(DateUtil.isValidDate(2020, 1, 31) == true);
 
-        MyDate[] date=new MyDate[1000];
+        MyDate date[] = new MyDate[1000];
         Random rand = new Random();
-        int year,month,day;
-        year=rand.nextInt(10000);
-        month=rand.nextInt(25);
-        day=rand.nextInt(50);
-        int count=0;
-        for(int i=0;i<1000;i++)
-        {
-            date[i]=new MyDate(year,month,day);
-            if(DateUtil.isValidDate(year,month,day))
-            {
-                System.out.println();
-            }
-            else count++;
-            year=rand.nextInt(10000);
-            month=rand.nextInt(25);
-            day=rand.nextInt(50);
+        int year, month, day;
+        int count = 0;
+        for (int i = 0; i < 1000; i++) {
+            year = rand.nextInt(2022);
+            month =rand.nextInt(12);
+            day = rand.nextInt(31);
+            date[i] = new MyDate(year, month, day);
+            if(DateUtil.isValidDate(year, month, day)) {
+                System.out.println(i+1+"-ik datum\n");
+                System.out.println(date[i].getYear());
+                System.out.println(date[i].getMonth());
+                System.out.println(date[i].getDay());
+                System.out.println(date[i].toString() + "\n");
+            } else count++;
         }
-        System.out.println();
+        System.out.println("NEm valid datumok:" + count);
     }
 }
